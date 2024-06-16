6bbdbe76df5b7301a82e760e0f169511cc498375
miguel <miguel.pinam@ginseg.com>
2024-06-16 19:58:24 +0200

    actualizacion de portfolio

--- proyectos.js
+++ proyectos.js
@@ -1,18 +1,18 @@
 // trabajos.js
 const proyectos = [
     {
-      titulo: "Juego 3 en raya LOCAS",
-      imagen: "juego.png",
-      descripcion: "Proyecto colaborativo junto a mi compañero Aaron en el cual desarrollamos un 3 en raya con la diferencia de que hay 3 tamaño de fichas, tres XL, tres L y tres S donde te puedes comer a las de menor tamaño además de un modo frenesí para limitar el tiempo de turno. También cuenta con pequeño de login para identificar a los jugadores.",
-      enlace: "https://659efa533163af2385f928dc--precious-cocada-4c9d37.netlify.app/",
-      github: "https://github.com/ItxChewy/3-en-rayas-locas",
+      titulo: "ConexionFit",
+      imagen: "conexionfit.png",
+      descripcion: "Este es mi proyecto de fin de grado junto a un compañero. El proyecto destaca por la interfaz amigable, el uso de librerías como Chart.JS para el uso de gráficas y porque es el primer proyecto en el que consigo que interactúen el frontend con un mi propio backend en un proyecto serio.",
+      enlace: "https://conexionfit.vercel.app/",
+      github: "https://github.com/piniass/tfg",
     },
     {
-      titulo: "Página de Gurises",
-      imagen: "Gurises.png",
-      descripcion: "Proyecto que seguimos en construcción de la página de Gurises, una agencia creativa que busca conectar con artistas. El proyecto sigue en desarrollo, faltan contenidos y diseño responsive",
-      enlace: "https://64ff3f20ef5814248ea46d15--heartfelt-choux-4a5793.netlify.app/",
-      github: "https://github.com/piniass/Gurises",
+      titulo: "Buscador de paises",
+      imagen: "flag.png",
+      descripcion: "Proyecto de Frontend Mentor desarrollado con React y Tailwind. El objetivo de este proyecto era desarrollar un pequeño trabajo realizando las mejores prácticas posibles con React usando prinicipios SOLID. También es la primera vez que diseño un switch de cambio de tema el cual he gestionado con el localstorage y Tailwind CSS.",
+      enlace: "https://flags-frontendmoentor.vercel.app/",
+      github: "https://github.com/piniass/flags-frontendmoentor",
     },
     {
         titulo: "Página de Marco Dimio",
@@ -22,34 +22,12 @@ const proyectos = [
         github: "#",
       },
     {
-        titulo: "Página de producto",
-        imagen: "tienda.png",
-        descripcion: "Desafio de FrontendMentor. Principal foco en el manejo del DOM. Su funcionalidad es unicamente a nivel cliente, no hay un backdend detrás.",
-        enlace: "https://delicate-llama-7ac70b.netlify.app/",
-        github: "https://github.com/piniass/ecommerce-product-page",
-      },
-      {
-        titulo: "CRUD con Python/Flask/SQL",
-        imagen: "crud-flask.png",
-        descripcion: "Práctica de clase de para familiarizarnos con Python, Flask y el acceso a base de datos, con un poco de Bootstrap.",
-        enlace: "https://practrica-flask-qjz1z40p5-piniass-projects.vercel.app/",
-        github: "https://github.com/piniass/practrica-flask",
-      },
-    {
       titulo: "Generador de Contraseñas",
       imagen: "generador.png",
       descripcion: "Desafio de FrontendMentor. Lo hice para practicar el maquetado y el manejo del JavaScript.",
       enlace: "https://64f4a5102d4755272dafcda8--superlative-sunburst-9512a3.netlify.app/",
       github: "https://github.com/piniass/generador-contrasenas",
     },
-    {
-        titulo: "Calculador de Propinas",
-        imagen: "propinas.png",
-        descripcion: "Desafio de FrontendMentor. Me centré en la maquetación y en que todo funcionase de la mejor manera.",
-        enlace: "https://64db875244a2fb685cd35924--imaginative-lolly-efd1c6.netlify.app/",
-        github: "https://github.com/piniass/tip-calculator",
-      },
-    
   ];
   
   export default proyectos;
